7d6f2c8ca43330e335995a7485ef054139bef8a5
Al Amin Khan <alaminkhan2010@gmail.com>
2025-12-18 01:36:20 +0600

    feat: get all books api

--- src/books.js
+++ src/books.js
@@ -1,13 +1,19 @@
-import express from 'express';
-import { getDB } from './db.js';
-
+import express from "express";
+import { getDB } from "./db.js";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
 router.get("/latest", async (req, res) => {
+    console.log(req.headers)
     try {
         const db = getDB();
-        const books = await db.collection("books").find({}).limit(6).toArray();
+        const books = await db
+            .collection("books")
+            .find()
+            .sort({ created_at: -1 })
+            .limit(8)
+            .toArray();
 
         res.json({
             success: true,
@@ -21,6 +27,29 @@ router.get("/latest", async (req, res) => {
         });
     }
 });
+
+router.get("/", async (req, res) => {
+    try {
+        const db = getDB();
+        const books = await db
+            .collection("books")
+            .find()
+            .sort({ created_at: -1 })
+            .toArray();
+
+        res.json({
+            success: true,
+            message: "All books fetched successfully",
+            data: books,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch all books",
+        });
+    }
+});
+
 
 
 export const booksRouter = router;
